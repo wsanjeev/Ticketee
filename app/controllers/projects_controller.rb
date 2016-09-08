e68fa53bc2b43604a3a1d5ca111335336f4cf0bf
Sanjeev <sanjeev.wangi@synechron.com>
2016-09-08 12:14:31 +0900

    create a new project feature complete

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,37 @@
+class ProjectsController < ApplicationController
+def index
+end
+
+def new
+@project=Project.new
+end
+
+def create
+
+@project=Project.new(project_params)
+
+params
+{
+}
+
+if @project.save
+
+flash[:notice]="Project has been created"
+redirect_to @project
+
+else
+
+#NOTHING YET
+end
+end
+
+def show
+@project=Project.find(params[:id])
+end
+private
+
+def project_params
+params.require(:project).permit(:name,:description)
+end
+end
+
